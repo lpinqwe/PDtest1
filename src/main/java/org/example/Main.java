1629e515e947838cd17b661bfc3e0ff27d70d72f
Volodar <gorelikovva@gmail.com>
2023-05-28 13:48:59 +0200

    binar is fasterererererer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,22 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
-        Student student = new Student("NAME_1",10);
-        student.addThing("math");
-        student.addThing("physics");
-        student.addThing("hitory");
-        System.out.println(student);
-      //  System.out.println("CALL_1: "+student.getName()+"\n"+student.getAge()+"\n" +student.getItems());
-        student.addThing("EN");
-        System.out.println(student);
-      //  System.out.println("CALL_2: "+student.getItems());
 
+        Q3 lists = new Q3();
+        lists.FillArr();
+        long start = System.nanoTime();
+
+        lists.perebor(100000000);
+
+        long finish = System.nanoTime();
+        long elapsed = finish - start;
+        System.out.println("Прошло времени, нс: " + elapsed);
+        start = System.nanoTime();
+        lists.binarySearch1(lists.list, 100000000-1, 0, lists.list.size());
+
+        finish = System.nanoTime();
+        elapsed = finish - start;
+        System.out.println("Прошло времени, нс: " + elapsed);
     }
 }
---
+++ src/main/java/org/example/Q3.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q3 {
+    List<Integer> list = new ArrayList<>();
+
+    public void FillArr() {
+        for (int i = 0; i < 100000000; i++) {
+            list.add(i);
+        }
+    }
+
+    public boolean perebor(int numb) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(numb)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static int binarySearch1(List<Integer> values, int valueToFind, int l, int r) {
+        if (l == r) {
+            return (values.get(l) == valueToFind) ? l : -1;
+        }
+
+        int m = l + (r - l) / 2;
+
+        if (valueToFind > values.get(m)) {
+            return binarySearch1(values, valueToFind, m + 1, r);
+        } else if (values.get(m) > valueToFind) {
+            return binarySearch1(values, valueToFind, l, m - 1);
+        }
+        return m;
+    }
+}
+
